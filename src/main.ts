4b2cf044a003bf22bff849a7cdea6772b6787e11
赵云龙 <zhaoyunlong@gridsum.com>
2023-01-30 10:27:38 +0800

    feat: 组件样式引入

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,14 @@ import registerIcons from './global/register-icons'
 // 样式
 import 'normalize.css'
 import './assets/css/index.less'
+// 1.全局引入
+// 2.单个组件引入
 // 对于逻辑中的组件，插件不会帮你导入，你可以引入全部样式，或者单独引入，或者使用别的插件
-import 'element-plus/theme-chalk/el-message.css'
-
+// 只引入el-message.css
+// import 'element-plus/theme-chalk/el-message.css'
+// 3.使用插件
+// npm i vite-plugin-style-import -D
+// npm i consola -D
 
 const app = createApp(App)
 app.use(registerIcons)
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// https://github.com/vbenjs/vite-plugin-style-import
+import {
+  createStyleImportPlugin,
+  ElementPlusResolve,
+} from 'vite-plugin-style-import'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,7 +19,23 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    // 帮助我们自动引入样式
+    createStyleImportPlugin({
+      resolves: [
+        ElementPlusResolve(),
+      ],
+      libs: [
+        // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `element-plus/theme-chalk/${name}.css`
+          },
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
