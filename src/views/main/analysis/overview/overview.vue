bbfddad1f6885836e8751caab7a81ecda5743639
yunbaba520 <1650620783@qq.com>
2023-02-05 11:20:01 +0800

    feat: 核心技术模块开发

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -1,12 +1,118 @@
 <template>
   <div class="overview">
-    <h2>overview</h2>
+    <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>关于</span>
+        </div>
+      </template>
+      <div>
+        Vue3Admin 是基于 Vue3、Vuex、VueRouter、Vite、 ElementPlus
+        、TypeScript、Echart5 等后台系统解决方案。
+      </div>
+    </el-card>
+    <el-card class="two-card">
+      <template #header>
+        <div class="card-header">
+          <span>技术栈</span>
+        </div>
+      </template>
+      <ul class="two-card-ul">
+        <template v-for="item in liList" :key="item.key">
+          <li class="two-card-li">
+            <span>{{ item.key }}</span>
+            <ElButton type="primary" link>{{ item.value }}</ElButton>
+          </li>
+        </template>
+      </ul>
+    </el-card>
   </div>
 </template>
 
-<script setup lang="ts" name="overview"></script>
+<script setup lang="ts" name="overview">
+import { reactive } from 'vue'
 
-<style scoped>
+const liList = reactive([
+  {
+    key: '开发工具 :',
+    value: 'Visual Studio Code'
+  },
+  {
+    key: '编程语言 :',
+    value: 'TypeScript 4.x + JavaScript'
+  },
+  {
+    key: '构建工具 :',
+    value: 'Vite 3.x'
+  },
+  {
+    key: '前端框架 :',
+    value: 'Vue 3.x'
+  },
+  {
+    key: '路由工具 :',
+    value: 'Vue Router 4.x'
+  },
+  {
+    key: '状态管理 :',
+    value: 'Pinia 2.x'
+  },
+  {
+    key: 'UI 框架 :',
+    value: 'Element Plus'
+  },
+  {
+    key: '可视化 :',
+    value: 'Echart5.x'
+  },
+  {
+    key: '富文本 :',
+    value: 'WangEditor'
+  },
+  {
+    key: '工具库 :',
+    value: '@vueuse/core + dayjs + countup.js'
+  },
+  {
+    key: 'CSS 预编译 :',
+    value: 'Less'
+  },
+  {
+    key: 'HTTP 工具',
+    value: 'Axios'
+  },
+  {
+    key: 'Git Hook 工具 :',
+    value: 'husky'
+  },
+  {
+    key: '代码规范 :',
+    value: 'EditorConfig + Prettier + ESLint'
+  },
+  {
+    key: '提交规范 :',
+    value: 'Commitizen + Commitlint'
+  },
+  {
+    key: '自动部署 :',
+    value: 'Centos + Jenkins + Nginx'
+  }
+])
+</script>
+
+<style scoped lang="less">
 .overview {
+  .card-header {
+    font-weight: 700;
+  }
+  .two-card {
+    margin-top: 20px;
+    .two-card-ul {
+      padding: 0 20px;
+      .two-card-li {
+        margin-top: 10px;
+      }
+    }
+  }
 }
 </style>
--- src/views/main/system/user/c-cpn/user-dialog.vue
+++ src/views/main/system/user/c-cpn/user-dialog.vue
@@ -2,7 +2,7 @@
   <div class="user-dialog">
     <el-dialog
       v-model="dialogVisible"
-      :title="isEditMy ? '编辑用户':'新建用户'"
+      :title="isEditMy ? '编辑用户' : '新建用户'"
       width="30%"
       @close="handlerDialogClose"
     >
@@ -12,23 +12,40 @@
             <el-input v-model="formData.name" placeholder="请输入用户名" />
           </el-form-item>
           <el-form-item label="真实姓名" prop="realname">
-            <el-input v-model="formData.realname" placeholder="请输入真实姓名" />
+            <el-input
+              v-model="formData.realname"
+              placeholder="请输入真实姓名"
+            />
           </el-form-item>
           <el-form-item label="登录密码" prop="password" v-if="!isEditMy">
-            <el-input v-model="formData.password" placeholder="请输入登录密码" />
+            <el-input
+              v-model="formData.password"
+              placeholder="请输入登录密码"
+            />
           </el-form-item>
           <el-form-item label="手机号码" prop="cellphone">
-            <el-input v-model="formData.cellphone" placeholder="请输入手机号码" />
+            <el-input
+              v-model="formData.cellphone"
+              placeholder="请输入手机号码"
+            />
           </el-form-item>
           <el-form-item label="选择角色" prop="roleId">
-            <el-select v-model="formData.roleId" placeholder="请选择角色" style="width: 100%">
+            <el-select
+              v-model="formData.roleId"
+              placeholder="请选择角色"
+              style="width: 100%"
+            >
               <template v-for="item in roleList" :key="item.id">
                 <el-option :value="item.id" :label="item.name" />
               </template>
             </el-select>
           </el-form-item>
           <el-form-item label="选择部门" prop="departmentId">
-            <el-select v-model="formData.departmentId" placeholder="请选择部门" style="width: 100%">
+            <el-select
+              v-model="formData.departmentId"
+              placeholder="请选择部门"
+              style="width: 100%"
+            >
               <template v-for="item in departmentList" :key="item.id">
                 <el-option :value="item.id" :label="item.name" />
               </template>
@@ -39,9 +56,7 @@
       <template #footer>
         <span class="dialog-footer">
           <el-button @click="dialogVisible = false">取消</el-button>
-          <el-button type="primary" @click="handlerSubmit">
-            确定
-          </el-button>
+          <el-button type="primary" @click="handlerSubmit"> 确定 </el-button>
         </span>
       </template>
     </el-dialog>
@@ -49,16 +64,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref ,reactive} from 'vue'
-import useDepartment from "@/stores/main/system/department";
-import useRole from "@/stores/main/system/role";
-import useUser from "@/stores/main/system/user";
-import { storeToRefs } from 'pinia';
-import { ElForm, ElMessage } from 'element-plus';
+import { ref, reactive } from 'vue'
+import useDepartment from '@/stores/main/system/department'
+import useRole from '@/stores/main/system/role'
+import useUser from '@/stores/main/system/user'
+import { storeToRefs } from 'pinia'
+import { ElForm, ElMessage } from 'element-plus'
 const departmentStore = useDepartment()
 const roleStore = useRole()
 const userStore = useUser()
-const emit = defineEmits(["refreshTable"])
+const emit = defineEmits(['refreshTable'])
 // 定义数据绑定
 const formData = reactive<any>({
   name: '',
@@ -71,11 +86,11 @@ const formData = reactive<any>({
 const isEditMy = ref<boolean>(false)
 const editDataMy = ref()
 // 部门，角色列表
-const {departmentList} = storeToRefs(departmentStore)
-const {roleList} = storeToRefs(roleStore)
+const { departmentList } = storeToRefs(departmentStore)
+const { roleList } = storeToRefs(roleStore)
 // 显示/隐藏
 let dialogVisible = ref<boolean>(false)
-function setDialogVisible(isEdit: boolean,editData?: any) {
+function setDialogVisible(isEdit: boolean, editData?: any) {
   isEditMy.value = isEdit
   editDataMy.value = editData
   if (isEdit && editData) {
@@ -92,14 +107,14 @@ async function handlerSubmit() {
   let res
   if (isEditMy.value) {
     // 编辑
-    res = await userStore.editUserAction(editDataMy.value.id,formData)
+    res = await userStore.editUserAction(editDataMy.value.id, formData)
   } else {
     // 新增
     res = await userStore.addUserAction(formData)
   }
   if (res.code === 0) {
     ElMessage.success(res.data)
-  }else {
+  } else {
     ElMessage.error(res.data)
   }
   // 表格刷新
@@ -111,7 +126,6 @@ function handlerDialogClose() {
   for (const key in formData) {
     formData[key] = ''
   }
-
 }
 defineExpose({
   setDialogVisible
